3b5235aaf012f782369db1f8a8668d245e3c05bd
John Ayling <johnayling@yahoo.com>
2018-04-19 18:49:36 -0500

    Add files via upload

---
+++ C-Sharp/Database/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Assignment3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sqlText = new System.Windows.Forms.TextBox();
+            this.executeButton = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.ListBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(145, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 36);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Department Data";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter SQL command";
+            // 
+            // sqlText
+            // 
+            this.sqlText.Location = new System.Drawing.Point(117, 46);
+            this.sqlText.Name = "sqlText";
+            this.sqlText.Size = new System.Drawing.Size(334, 20);
+            this.sqlText.TabIndex = 1;
+            this.sqlText.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Enter_Pressed);
+            // 
+            // executeButton
+            // 
+            this.executeButton.Location = new System.Drawing.Point(457, 44);
+            this.executeButton.Name = "executeButton";
+            this.executeButton.Size = new System.Drawing.Size(75, 23);
+            this.executeButton.TabIndex = 2;
+            this.executeButton.Text = "Execute";
+            this.executeButton.UseVisualStyleBackColor = true;
+            this.executeButton.Click += new System.EventHandler(this.ExecuteButton_Click);
+            // 
+            // output
+            // 
+            this.output.BackColor = System.Drawing.Color.Black;
+            this.output.Font = new System.Drawing.Font("Arial monospaced for SAP", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.ForeColor = System.Drawing.Color.Lime;
+            this.output.FormattingEnabled = true;
+            this.output.HorizontalScrollbar = true;
+            this.output.ItemHeight = 12;
+            this.output.Location = new System.Drawing.Point(13, 73);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(519, 184);
+            this.output.TabIndex = 5;
+            this.output.TabStop = false;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(15, 268);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(520, 23);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(237, 297);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(544, 332);
+            this.ControlBox = false;
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.executeButton);
+            this.Controls.Add(this.sqlText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Assignment 3";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox sqlText;
+        private System.Windows.Forms.Button executeButton;
+        private System.Windows.Forms.ListBox output;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ C-Sharp/Database/Form1.cs
@@ -0,0 +1,366 @@
+﻿/*
+ *
+ *  Program:    Assignment 3
+ *  Author:     John Ayling
+ *  Due:        March 7th
+ *  Purpose:    To surply a graphical interface to the database.  At this time it only select and insert
+ *  commands are functional as per the assignment
+ */
+using System;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.IO;
+using System.Text;
+
+namespace Assignment3
+{
+    /*
+     * our form class
+     */
+    public partial class Form1 : Form
+    {
+        //constant for connection string
+        private const String CONN = "server=10.158.56.53;uid=csci473g21;pwd=wordpass21;database=csci473g21;";
+
+        /*
+         * Constructor for the form
+         */
+        public Form1()
+        {
+            InitializeComponent();
+        }//end Form1
+
+        /*
+         * Called when the exit button is clicked
+         */
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }//end ExitButton_Click
+
+        /*
+         * Called when user presses a key from the text field
+         * calls Execute code if that key was Enter
+         */
+        private void Enter_Pressed(object sender,KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                ExecuteButton_Click(this, new EventArgs());
+        }//end Enter_Pressed
+
+        /*
+         * Called when the clear button is clicked
+         */
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            //clear the fields
+            output.Items.Clear();
+            sqlText.Text = String.Empty;
+
+            //redisplay the tables
+            ShowTables();
+        }//end ClearButton_Click
+
+        /*
+         * Called at the start of the form
+         */
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            //create the connection
+            using (MySqlConnection connection = new MySqlConnection(CONN))
+            {
+                MySqlCommand command;
+                String line;
+                String commandStr;
+                String[] values;
+                char[] splitChar = { ',' };
+
+                try
+                {
+                    //open connection
+                    connection.Open();
+
+                    //drop class table if it exists
+                    command = new MySqlCommand("drop table if exists ClassTable", connection);
+                    command.ExecuteNonQuery();
+
+                    //create class table
+                    command = new MySqlCommand("create table ClassTable(" +
+                        "Class varchar(20) not null," +
+                        "Teacher varchar(20)," +
+                        "Room int(5)," +
+                        "Time varchar(5)," +
+                        "Days varchar(5)," +
+                        "Enrollment int(5)," +
+                        "primary key(Class)" +
+                        ")", connection);
+                    command.ExecuteNonQuery();
+
+                    //fill class table
+                    using (StreamReader SR = new StreamReader("Class.txt"))
+                    {
+                        //get the first line
+                        line = SR.ReadLine();
+
+                        while(line !=null)
+                        {
+                            values = line.Split(splitChar);
+
+                            //create the string
+                            commandStr = "Insert into ClassTable values('" + values[0] + "','" +
+                                values[1] + "','" +
+                                values[2] + "','" +
+                                values[3] + "','" +
+                                values[4] + "','" +
+                                values[5] + 
+                                "');";
+
+                            //create and execute the command
+                            command = new MySqlCommand(commandStr, connection);
+                            command.ExecuteNonQuery();
+                            line = SR.ReadLine();
+                        }//end while
+                    }//end using
+
+                    //drop office table if it exists
+                    command = new MySqlCommand("drop table if exists OfficeTable", connection);
+                    command.ExecuteNonQuery();
+
+                    //create office table
+                    command = new MySqlCommand("create table OfficeTable(" +
+                        "Teacher varchar(20) not null," +
+                        "Office int(5)," +
+                        "primary key(Teacher)" +
+                        ")", connection);
+                    command.ExecuteNonQuery();
+
+                    //fill office table
+                    using (StreamReader SR = new StreamReader("Office.txt"))
+                    {
+                        //get the first line
+                        line = SR.ReadLine();
+
+                        while (line != null)
+                        {
+                            values = line.Split(splitChar);
+
+                            //create the string
+                            commandStr = "Insert into OfficeTable values('" + values[0] + "','" +
+                                values[1] + "');";
+
+                            //create and execute the command
+                            command = new MySqlCommand(commandStr, connection);
+                            command.ExecuteNonQuery();
+                            line = SR.ReadLine();
+                        }//end while
+                    }//end using
+
+                    //drop room table if it exists
+                    command = new MySqlCommand("drop table if exists RoomTable", connection);
+                    command.ExecuteNonQuery();
+
+                    //create room table
+                    command = new MySqlCommand("create table RoomTable(" +
+                        "Room int(5) not null," +
+                        "Capacity int(5)," +
+                        "Smart char(1)," +
+                        "primary key(Room))", connection);
+                    command.ExecuteNonQuery();
+
+                    //fill room table
+                    using (StreamReader SR = new StreamReader("Room.txt"))
+                    {
+                        //get the first line
+                        line = SR.ReadLine();
+
+                        while (line != null)
+                        {
+                            values = line.Split(splitChar);
+
+                            //create the string
+                            commandStr = "Insert into RoomTable values('" + values[0] + "','" +
+                                values[1] + "','" +
+                                values[2] + "'" +
+                                ");";
+
+                            //create and execute the command
+                            command = new MySqlCommand(commandStr, connection);
+                            command.ExecuteNonQuery();
+                            line = SR.ReadLine();
+                        }//end while
+                    }//end using
+
+                    //show tables
+                    ShowTables();
+                }//end try
+                catch (MySqlException ex)
+                {
+                    //will print the message connected to the exception
+                    output.Items.Add(ex.Message);
+                    output.Items.Add(String.Empty);
+                }//end catch
+                finally
+                {
+                    //close connection if there
+                    if(connection!=null)
+                        connection.Close();
+                }
+            }//end using
+        }//end Form1_Load
+
+        /*
+         * Called when the execute button is clicked
+         */
+        private void ExecuteButton_Click(object sender, EventArgs e)
+        {
+            String query = sqlText.Text;
+            StringBuilder line = new StringBuilder();
+            char[] space = { ' ','(' };
+            String[] values;
+            MySqlCommand command;
+            MySqlDataReader reader;
+
+            //check is empty
+            if (query.Length > 0)
+            {
+                //make a connection
+                using (MySqlConnection connection = new MySqlConnection(CONN))
+                {
+                    try
+                    {
+                        //open connection
+                        connection.Open();
+
+                        //create the command
+                        command = new MySqlCommand(query, connection);
+
+                        //split the sting
+                        values = query.Split(space);
+
+                        //select path
+                        if (values[0].ToLower().Equals("select"))
+                        {
+                            //check if count was used
+                            if (values[1].ToLower().Equals("count"))
+                            {
+                                line.Append("Qty is " + command.ExecuteScalar());
+                                output.Items.Add("Result of query:");
+                                output.Items.Add(line);
+                            }//end if
+                            else
+                            {
+                                //execute the command
+                                reader = command.ExecuteReader();
+
+                                //clear the line
+                                line.Clear();
+
+                                //display column header
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    line.Append(reader.GetName(i).PadRight(20));
+                                }//end for
+
+                                //add line to the display
+                                output.Items.Add(line);
+
+                                //while there is data
+                                while (reader.Read())
+                                {
+                                    //clear the line
+                                    line.Clear();
+
+                                    //build the line
+                                    //for (int i=0; i < reader.FieldCount; i++)
+                                    //  line.Append(reader.GetString(i).PadRight(20));
+                                    IDataRecord record = (IDataRecord)reader;
+
+                                    for(int i = 0; i<record.FieldCount;i++)
+                                        line.Append(record[i].ToString().PadRight(20));
+
+                                    //add the line
+                                    output.Items.Add(line);
+                                }//end while
+
+                                //close reader
+                                reader.Close();
+
+                            }//end else
+                        }//end if
+                        //for insert commands
+                        else if (values[0].ToLower().Equals("insert"))
+                        {
+                            line.Append(command.ExecuteNonQuery() + " lines affected");
+                            output.Items.Add("Result of query:");
+                            output.Items.Add(line);
+                        //for all other commands
+                        }//end else if
+                        else
+                        {
+                            output.Items.Add("That command is not currently available");
+                        }//end else
+                    }//end try
+                    //will display ant errors to output
+                    catch (MySqlException ex)
+                    {
+                        //print out any errors
+                        output.Items.Add(ex.Message);
+                    }//end catch
+                    finally
+                    {
+                        //for spacing
+                        output.Items.Add(String.Empty);
+
+                        //focuses on the last line
+                        output.SelectedIndex = output.Items.Count - 1;
+
+                        //close connection if exists
+                        if (connection!=null)
+                            connection.Close();
+                    }
+                }//end using
+            }//end if
+        }//end executeButton
+
+        private void ShowTables()
+        {
+            using (MySqlConnection connection = new MySqlConnection(CONN))
+            {
+                try
+                {
+                    connection.Open();
+
+                    //get schema
+                    DataTable schema = connection.GetSchema("Tables");
+
+                    //print out tables
+                    output.Items.Add("Tables in database");
+                    foreach (DataRow row in schema.Rows)
+                    {
+                        output.Items.Add(row[2].ToString());
+                    }//end foreach
+
+                }//end try
+                catch (MySqlException ex)
+                {
+                    //print out any errors
+                    output.Items.Add(ex.Message);
+                }//end catch
+                finally
+                {
+                    //for spacing
+                    output.Items.Add(String.Empty);
+
+                    //focuses on the last line
+                    output.SelectedIndex = output.Items.Count - 1;
+
+                    //close connection if exists
+                    if (connection!= null)
+                        connection.Close();
+
+                }//end finally
+            }//end using
+        }//show tables
+    }//end class Form1
+}//end namespace
